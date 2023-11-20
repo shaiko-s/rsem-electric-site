0091402399fef48436667fea76ddff8a96f3e3fe
Sergey Shaiko <foodprofi@gmail.com>
2023-11-20 23:24:49 +0100

    Add font imports and update app styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,14 @@
 import './bootstrap';
 
+// Import Fonts
+import '../sass/_fonts.sass';
+
+// Import variables
+// import '../sass/_variables.sass';
+
+//import '../css/reset.css'; // CSS Reset, but maybe not needed because of Tailwind
+import '../sass/app.sass';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
