9f42c023f984c7f9cafbb8788dbb0321637ded01
Sergey Shaiko <foodprofi@gmail.com>
2023-11-24 20:47:44 +0100

    Add footer layout with menu and contact information

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,73 @@
+<div class="footer-menu">
+    <div class="footer-menu__wrapper">
+        <div class="footer-menu__company">
+            <x-application-logo />
+            <p>Электромонтажные<br> работы полного цикла</p>
+        </div>
+
+        <div class="footer-menu__services">
+            <h6 class="footer-menu__header">Услуги</h6>
+            <ul class="footer-menu__list">
+                <li>Услуга</li>
+                <li>Услуга</li>
+                <li>Услуга</li>
+                <li>Услуга</li>
+                <li>Услуга</li>
+                <li>Услуга</li>
+            </ul>
+        </div>
+
+        <div class="footer-menu__solutions">
+            <h6 class="footer-menu__header">Решения</h6>
+            <ul class="footer-menu__list">
+                <li>Решение</li>
+                <li>Решение</li>
+                <li>Решение</li>
+                <li>Решение</li>
+                <li>Решение</li>
+                <li>Решение</li>
+            </ul>
+        </div>
+
+        <div class="footer-menu__projects">
+            <h6 class="footer-menu__header">Проекты</h6>
+            <ul class="footer-menu__list">
+                <li>Проект</li>
+                <li>Проект</li>
+                <li>Проект</li>
+                <li>Проект</li>
+            </ul>
+        </div>
+
+        <div class="footer-menu__contacts">
+            <h6 class="footer-menu__header">Контакты</h6>
+
+            <ul class="footer-menu__list">
+                <li class="contacts-phone">
+                    <span class="contacts-title">Телефон:</span>
+                    <span class="contacts-info">+7 (495) 999 88 77</span>
+                </li>
+                <li class="contacts-address">
+                    <span class="contacts-title">Адрес:</span>
+                    <span class="contacts-info">Московская область,
+                        г.Балашиха, мкр.
+                        Железнодорожный
+                        ул. Новая 22а</span>
+                    </li>
+                <li class="contacts-email">
+                    <span class="contacts-title">E-mail:</span>
+                    <span class="contacts-info">info@rse-montag.ru</span>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+</div>
+<div class="footer-copyright">
+    <div class="footer-copyright__wrapper">
+        <span>&copy; 2023 Рем Строй Энерго Монтаж</span>
+        <a href="#">Политика конфиденциальности</a>
+        <a href="#">Карта сайта</a>
+    </div>
+</div>
