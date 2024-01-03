329227ca066faa3308134d3828c6b714902ae5ce
Medju2514 <rinat.shaiko.81@gmail.com>
2024-01-03 23:11:46 +0100

    about company and minor fixes in main

---
+++ resources/views/pages/about-company.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <div class="_container mx-auto">
+        <x-slot name="header">
+            @include('layouts.header')
+        </x-slot>
+        <div class="about-top">
+            <div class="content-wrapper mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 class="about-top__title">
+                    О Компании
+                </h1>
+
+                <p class="white-text">
+                    Рыбатекст используется дизайнерами, проектировщиками и фронтендерами, когда нужно быстро заполнить макеты или прототипы содержимым. Это тестовый контент, который не должен нести никакого смысла, лишь показать наличие самого текста или продемонстрировать типографику в деле.
+                </p>
+            </div>
+        </div>
+        
+        <div class="electro-project">
+            <div class="content-wrapper mx-auto px-4 sm:px-6 lg:px-8">
+                <h2>
+                    Реализуйте свой проект вместе с РСЭМ
+                </h2>
+                <x-buttons.btn-blue title="К проектам" id="btn-to-projects" />
+            </div>
+        </div>
+        <x-slot name="footer">
+            @include('layouts.footer')
+        </x-slot>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('pages/main');
 });
 
+Route::get('/about', function () {
+    return view('pages/about-company');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
